8d5d61b6b7439521dcce43ef031cdb8850546d90
John Rowley <outsidethebox@eircom.net>
2022-11-08 21:11:56 +0000

    Final code drop of the evening

--- week2/js/list2.js
+++ week2/js/list2.js
@@ -27,7 +27,7 @@ function initDwarves() {
 
 function listDwarves(dwarves) {
 
-    for(let counter = 0; counter < dwarves.length; counter++) {
+    for (let counter = 0; counter < dwarves.length; counter++) {
 
         console.log(`The dwarf at position ${counter} is ${dwarves[counter]}`)
     }
@@ -55,15 +55,36 @@ function DisneyDwarves() {
 
     listDwarves(dwarves)
 
-
+    SendDwarvesHome(dwarves);
 }
 
 DisneyDwarves()
 
-//Imagine you have full list of dwarves
-//And you have an a bus for EACH dwarf [GetBus(dwarf)]
-//and you have to call the bus for each dwarf to get them home
+// Imagine you have full list of dwarves
+// And you have an a bus for EACH dwarf [GetBus(dwarf)]
+// and you have to call the bus for each dwarf to get them home
 // until there are no dwarves left to go home
 // show how many dwarves are left as each one leaves
-
 // Or to put it another way: while there dwarves left top pop off home, get them a bus
+
+function SendDwarvesHome(dwarves) {
+
+    console.log(`Queueing to get home`)
+    while (dwarves.length > 0) {
+
+        let dwarf = dwarves.pop();
+        GetBus(dwarf);
+        if (dwarves.length > 0) {
+            console.log(`After popping ${dwarf} : ${dwarves.join()} are left`)
+        }
+
+    }
+
+    console.log(`Everyone is gone home`)
+
+}
+
+function GetBus(dwarf) {
+
+    console.log(`${dwarf} has caught the bus home`)
+}
